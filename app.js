e3040a3a40eb05902cb75b74ff611ce5f3186063
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-19 09:14:59 +0300

    Delete Book From List

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ UI.prototype.showAlert = function(message, className) {
     }, 3000);
 }
 
+//  Delete Book
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+}
+
 //  Clear Fields
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
@@ -50,7 +57,7 @@ UI.prototype.clearFields = function() {
     document.getElementById('isbn').value = '';
 }
 
-//  Event Listeners
+//  Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
     const title = document.getElementById('title').value,
           author = document.getElementById('author').value,
@@ -76,6 +83,20 @@ document.getElementById('book-form').addEventListener('submit', function(e){
         //  Clear fields
         ui.clearFields();
     }
+
+    e.preventDefault();
+});
+
+//  Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+    
+    // Instantiate UI
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+
+    //  Show alert
+    ui.showAlert('Book removed!', 'success');
 
     e.preventDefault();
 });
