cd8dc144e89ddb16c091f52d296913b0c6695369
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-20 09:20:15 +0300

    Bonus - Add Local Storage

--- appes6.js
+++ appes6.js
@@ -54,6 +54,54 @@ class UI {
     }
 }
 
+//  Local Storage class
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        books.forEach(function(book){
+            const ui = new UI;
+
+            //  Add book to UI
+            ui.addBookToList(book);
+        });
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function (book, index) {
+            if(book.isbn === isbn) {
+                books.splice(index, 1)
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+//  DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks());
+
 //  Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
     const title = document.getElementById('title').value,
@@ -74,6 +122,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
         //  Add book to list
         ui.addBookToList(book);
 
+        //  Add book to Local Storage
+        Store.addBook(book);
+
         //  Show success
         ui.showAlert('Book Added!', 'success');
 
@@ -90,7 +141,11 @@ document.getElementById('book-list').addEventListener('click', function(e) {
     // Instantiate UI
     const ui = new UI();
 
+    //  Delete a book
     ui.deleteBook(e.target);
+
+    //  Remove from Local Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     //  Show alert
     ui.showAlert('Book removed!', 'success');
